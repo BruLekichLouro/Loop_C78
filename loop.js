53aa9f608009d7632c47381ebcef4c3fb9d70014
Bruna Lekich Louro <brunalekich@gmail.com>
2022-11-10 13:43:12 -0300

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,11 @@
+//Percorrendo e imprimindo - loop for:
+
+//criar variável arrayValor
+
+for(i=0; i < frutas.length; i++){
+    //imprimir frutas[i] no console
+    //arrayValor = arrayValor + frutas[i] + "<br>";
+}
+
+//Imprimir no html
+document.getElementById("array").innerHTML = arrayValor;
